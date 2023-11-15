6bef50130248f57c5eaa88b32507a52cfcd4b62f
OrionNavattan <103385399+OrionNavattan@users.noreply.github.com>
2023-11-15 08:47:18 -0800

    Strip trailing whitespace
    Add a bit of personal speculation related to a hardware variant
    A couple terminology changes

--- Addresses and Macros.asm
+++ Addresses and Macros.asm
@@ -6,17 +6,17 @@
 vdp_data_port:		equ $C00000
 vdp_control_port:	equ $C00004
 	dma_status_bit:		equ 1				; 1 if a DMA is in progress
-	
+
 	; VDP register settings
 	vdp_mode_register1:	equ $8000
 	vdp_md_color:		equ vdp_mode_register1+4	; Mega Drive colour mode
-	
+
 	vdp_mode_register2:	equ $8100
 	vdp_enable_display:	equ vdp_mode_register2+$40	; if not set, fill display with bg colour
 	vdp_enable_dma:		equ vdp_mode_register2+$10	; enable DMA operations
 	vdp_ntsc_display:	equ vdp_mode_register2		; 224px screen height (NTSC)
 	vdp_md_display:		equ vdp_mode_register2+4	; mode 5 Mega Drive display
-	
+
 	vdp_fg_nametable:	equ $8200			; fg (plane A) nametable setting
 	vdp_window_nametable:	equ $8300			; window nametable setting
 	vdp_bg_nametable:	equ $8400			; bg (plane B) nametable setting
@@ -26,23 +26,23 @@ vdp_control_port:	equ $C00004
 	vdp_sms_hscroll:	equ $8800
 	vdp_sms_vscroll:	equ $8900
 	vdp_hint_counter:	equ $8A00			; number of lines between horizontal interrupts
-	
+
 	vdp_mode_register3:	equ $8B00
 
 	vdp_full_vscroll:	equ vdp_mode_register3		; full screen vertical scroll mode
 	vdp_full_hscroll:	equ vdp_mode_register3		; full screen horizontal scroll mode
-	
+
 	vdp_mode_register4:	equ $8C00
 	vdp_320px_screen_width:	equ vdp_mode_register4+$81	; 320px wide screen mode
-	
+
 	vdp_hscroll_table:	equ $8D00			; horizontal scroll table setting
 	vdp_nametable_hi:	equ $8E00			; high bits of fg/bg nametable settings for 128kB VRAM
 	vdp_auto_inc:		equ $8F00			; value added to VDP address after each write
-	
+
 	vdp_plane_size:		equ $9000			; fg/bg plane dimensions
 	vdp_plane_height_32:	equ vdp_plane_size		; height = 32 cells (256px)
 	vdp_plane_width_64:	equ vdp_plane_size+1		; width = 64 cells (512px)
-	
+
 	vdp_window_x_pos:	equ $9100
 	vdp_window_y_pos:	equ $9200
 	vdp_dma_length_low:	equ $9300
@@ -64,7 +64,7 @@ console_version:	equ $A10001
 z80_bus_request:	equ $A11100
 z80_reset:			equ $A11200
 
-; TMSS registers 
+; TMSS registers
 tmss_sega:			equ $A14000			; 'SEGA' is written here to unlock the VDP
 tmss_bankswitch:	equ $A14101				; bit 0 switches between TMSS ROM and cart slot
 
@@ -95,17 +95,17 @@ vram_sprites:	equ	$D800
 vram_hscroll:	equ	$DC00
 
 widthof_cell:			equ	8			; width of single tile in pixels
-sizeof_vram_row_64:		equ (512/widthof_cell)*2	; $80,  single row of fg/bg nametable when 64 cells (512 pixels) wide 
+sizeof_vram_row_64:		equ (512/widthof_cell)*2	; $80,  single row of fg/bg nametable when 64 cells (512 pixels) wide
 
 vram_LicenseFont:	equ	$C20				; location in VRAM where license font is loaded
 
 RAM_Program_Start: 	equ	$FFFFC000			; location in RAM where TMSS test code is copied
 
 stack_pointer:		equ	$FFFF00				; initial stack pointer value
-						
+
 sizeof_LicenseFont:	equ	filesize("License Text Font.bin")
 
-; Flag values used in license text strings
+; Flags used in license text strings
 endline:	equ $FF
 endstring:	equ 0
 
@@ -117,7 +117,7 @@ ifarg:		macros
 		if strlen("\1")>0
 
 ; ---------------------------------------------------------------------------
-; Make a 68K instruction with a VDP command longword or word as the source 
+; Make a 68K instruction with a VDP command longword or word as the source
 ; (more or less replicating the vdpComm function in Sonic 2 AS)
 ; input: 68k instruction mnemonic, VRAM/VSRAM/CRAM offset, destination RAM
 ; (vram/vsram/cram), operation (read/write/dma), destination of 68K instruction,
@@ -128,7 +128,7 @@ vdp_comm:	macro inst,addr,cmdtarget,cmd,dest,adjustment
 
 		local type
 		local rwd
-	
+
 		if stricmp ("\cmdtarget","vram")
 		type: =	$21					; %10 0001
 		elseif stricmp ("\cmdtarget","cram")
@@ -137,7 +137,7 @@ vdp_comm:	macro inst,addr,cmdtarget,cmd,dest,adjustment
 		type: = $25					; %10 0101
 		else inform 2,"Invalid VDP command destination (must be vram, cram, or vsram)."
 		endc
-	
+
 		if stricmp ("\cmd","read")
 		rwd: =	$C					; %00 1100
 		elseif stricmp ("\cmd","write")
@@ -147,18 +147,18 @@ vdp_comm:	macro inst,addr,cmdtarget,cmd,dest,adjustment
 		else inform 2,"Invalid VDP command type (must be read, write, or dma)."
 		endc
 
-		ifarg \dest			
+		ifarg \dest
 			\inst\.\0	#(((type&rwd)&3)<<30)|((addr&$3FFF)<<16)|(((type&rwd)&$FC)<<2)|((addr&$C000)>>14)\adjustment\,\dest
-		else	
-			\inst\.\0	(((type&rwd)&3)<<30)|((addr&$3FFF)<<16)|(((type&rwd)&$FC)<<2)|((addr&$C000)>>14)\adjustment\	
+		else
+			\inst\.\0	(((type&rwd)&3)<<30)|((addr&$3FFF)<<16)|(((type&rwd)&$FC)<<2)|((addr&$C000)>>14)\adjustment\
 		endc
 		endm
-		
+
 ; ---------------------------------------------------------------------------
 ; Make a size constant for an assembly array.
 ; input: array start label
-; ---------------------------------------------------------------------------	
+; ---------------------------------------------------------------------------
 
 arraysize:	macro	arrayname
 sizeof_\arrayname: equ	*-\arrayname
-		endm						
+		endm
--- tmss.asm
+++ tmss.asm
@@ -1,16 +1,16 @@
 ; =========================================================================
-; Sega Mega Drive/Genesis Trademark Security System Bootrom 
+; Sega Mega Drive/Genesis Trademark Security System Bootrom
 ; Original source code (C) 1990 SEGA Corporation
 
-; Disassembled for SNASM68K by moxniso using Ghidra v9.2.1 (https://ghidra-sre.org/) 
+; Disassembled for SNASM68K by moxniso using Ghidra v9.2.1 (https://ghidra-sre.org/)
 ; Converted to ASM68K, cleaned up, and labeled/commented by OrionNavattan
 ; Thanks to Hivebrain for the VDP registers and inspiration for the styling,
 ; and MarkeyJester for information about the comma in the message text.
-; =========================================================================
+; ==========================================================================
 
 
 		opt	l.					; . is the local label symbol
-		opt	ae-					; automatic evens disabled 
+		opt	ae-					; automatic evens disabled
 		opt	ws+					; allow statements to contain white-spaces
 		opt	w+					; print warnings
 
@@ -24,12 +24,12 @@ ROM_Start:
 
 ROM_Header:
 		dc.b "SEGA GENESIS    "				; hardware system ID (Console name)
-        dc.b "(C)SEGA 1990.MAY"					; copyright holder and release date 
-        dc.b "GENESIS OS                                      "	; Domestic name                      
-        dc.b "GENESIS OS                                      "	; international name 
+        dc.b "(C)SEGA 1990.MAY"					; copyright holder and release date
+        dc.b "GENESIS OS                                      "	; Domestic name
+        dc.b "GENESIS OS                                      "	; international name
         dc.b "OS 00000000-00"					; serial/version number
         dc.w $5B74						; checksum; interestingly, it does NOT match the ROM. This might be from sample code or from an earlier build of the ROM.
-        dc.b "                "					; I/O support 
+        dc.b "                "					; I/O support
         dc.l ROM_Start						; start address of ROM
         dc.l ROM_End-1						; end address of ROM
         dc.l ram_start						; start address of RAM
@@ -40,69 +40,76 @@ ROM_Header:
         dc.b "U               "					; region (Country code), only North America is set despite this ROM being used worldwide (this may be a leftover from when TMSS was meant to be a region lockout)
 ; =========================================================================
 
-ErrorTrap: 
+ErrorTrap:
 		bra.s ErrorTrap					; any CPU exceptions that occur while bankswitched to the TMSS ROM are dumped in this infinite loop
 
 ; ---------------------------------------------------------------------------
-; This is a stripped-down version of the standard ICD_BLK4 init library.
-; it does not clear the main RAM (which isn't necessary here since that was a
-; workaround for a hardware bug on the Model 1 VA0), nor does it feed the usual 
-; RAM and register clearing program to the Z80, although values related to each 
-; of those are still loaded into the registers. Curiously, it, and the main 
-; test program, also seem to account for a hypothetical hardware revision that 
-; features this bootrom but NOT the VDP DTACK/RESET lock mechanism.
+; This is a stripped-down version of the standard ICD_BLK4 init library. it
+; does not clear the main RAM (which isn't necessary here since that was a
+; workaround for a hardware bug on the Model 1 VA0), nor does it feed the
+; usual RAM and register clearing program to the Z80, although values related
+; to each of those are still loaded into the registers. Curiously, it, and
+; the main test program, also seem to account for a hypothetical hardware
+; revision that features this bootrom but NOT the VDP DTACK/RESET lock
+; mechanism.
+
+; (Interestingly, descriptions of a prototype TMSS unit (Genesis III)
+; displayed in 1990, plus the fact that Accolade was apparently unaware that
+; production TMSS units displayed the license message until after they made
+; their games TMSS compliant, seems to imply that there was also a variant
+; that had the lock mechanism, but NOT this bootrom.)
 ; ---------------------------------------------------------------------------
 
 EntryPoint:
-		lea	SetupValues(pc),a5			; load setup array	
+		lea	SetupValues(pc),a5			; load setup array
         movem.l (a5)+,d5-a4					; first VDP register value, clear_ram loop counter (unused), VDP register increment, Z80 RAM start (unused), Z80 bus request register, Z80 reset register, VDP data port, VDP control port
-        move.b  console_version-z80_bus_request(a1),d0		; get console version 
+        move.b  console_version-z80_bus_request(a1),d0		; get console version
         andi.b  #console_revision,d0				; only need version bits
         beq.s   .no_hardware_lock				; if this is a (hypothetical) unit without the VDP lock, branch
-        move.l  #'SEGA',tmss_sega-z80_bus_request(a1)		; satisfy the TMSS (yes, this bootrom has to do this too) 
+        move.l  #'SEGA',tmss_sega-z80_bus_request(a1)		; satisfy the TMSS (yes, this bootrom has to do this too)
 
 	.no_hardware_lock:
 		move.w  (a4),d0					; clear write-pending flag in VDP to prevent issues if the 68k has been reset in the middle of writing a command long word to the VDP
         moveq   #0,d0
         movea.l d0,a6
         move.l  a6,usp ; set user stack pointer to 0
-        	
+
 		moveq   #sizeof_SetupVDP-1,d1			; number of VDP registers to write
 	.setup_vdp_regs:
 		move.b	(a5)+,d5				; add $8000 to value
 		move.w	d5,(a4)					; move value to	VDP register
 		add.w	d7,d5					; next register
         dbf	d1,.setup_vdp_regs				; repeat for all registers
-        
+
 		move.l  #$40000080,(a4)				; clear VRAM with VDP fill command
         move.w  d0,(a3)
 
 	.wait_for_dma:
-		move.w  (a4),d4					; get status register	
+		move.w  (a4),d4					; get status register
         btst	#dma_status_bit,d4
         bne.s	.wait_for_dma					; wait until the VRAM clear has finished
-        	
+
 		move.l	#(vdp_md_display<<16)|(vdp_auto_inc+2),(a4) ; set VDP autoincrement to 2 (one word) and disable DMA
         vdp_comm.l	move,0,cram,write,(a4)			; set VDP to CRAM write
         moveq   #(sizeof_cram/4)-1,d3				; set loop counter to $1F
 	.clear_cram:
 		move.l  d0,(a3)					; clear the CRAM
         dbf	d3,.clear_cram
-        	
-		vdp_comm.l	move,0,vsram,write,(a4)			; set VDP to VSRAM write
+
+		vdp_comm.l	move,0,vsram,write,(a4)		; set VDP to VSRAM write
         moveq	#(sizeof_vsram/4)-1, d4				; set loop counter to $13
 	.clear_vsram:
 		move.l  d0,(a3)					; clear the VSRAM
         dbf     d4,.clear_vsram
-        	
+
 		moveq	#4-1,d5					; number of PSG channels
 	.loop_psg:
 		move.b  (a5)+,psg_input-vdp_data_port(a3)	; mute all PSG channels
         dbf d5,.loop_psg
-        
+
 		bra.s   LoadTestProgram
-; =========================================================================	
-	
+; =========================================================================
+
 SetupValues:
 		dc.l    vdp_mode_register1			; d5
         dc.l    (sizeof_ram/4)-1				; d6, unused
@@ -133,34 +140,34 @@ SetupValues:
         dc.b	(vdp_plane_width_64|vdp_plane_height_32)&$FF	; $90: 64x32 plane size
         dc.b	vdp_window_x_pos&$FF				; $91: Window Plane X pos
         dc.b	vdp_window_y_pos&$FF				; $92: Window Plane Y pos
-		dc.w 	sizeof_vram-1				; $93/94: DMA length	
-        dc.w	0						; $95/96: DMA source	
+		dc.w 	sizeof_vram-1				; $93/94: DMA length
+        dc.w	0						; $95/96: DMA source
         dc.b 	vdp_dma_vram_fill&$FF				; $97: DMA fill
-    
+
     	arraysize	SetupVDP
 
-		dc.b	tPSG1|$1F,tPSG2|$1F,tPSG3|$1F,tPSG4|$1F	; PSG mute values	
+		dc.b	tPSG1|$1F,tPSG2|$1F,tPSG3|$1F,tPSG4|$1F	; PSG mute values
 ; =========================================================================
 
 LoadTestProgram:
 		lea	(RAM_Program_Start).w,a0		; RAM address to copy test code to
-        lea	Test_Registers(pc),a1				; array with register values to be used during test and displaying the license message
+        lea	Test_Registers(pc),a1				; array with register values to be used during test and loading the license message
         movem.l	(a1)+,d4-d7/a2-a6				; set new registers
-        
+
 		move.w	#(sizeof_RAM_Code/2)-1,d0		; set loop counter to $3F
-	.copy_to_ram: 
-		move.w	(a1)+,(a0)+				; copy test program to RAM  
-		dbf	d0,.copy_to_ram	
-	
+	.copy_to_ram:
+		move.w	(a1)+,(a0)+				; copy test program to RAM
+		dbf	d0,.copy_to_ram
+
 		jsr	(RAM_Program_Start).w			; jump to the code we just copied; we will not be returning here if the cartridge passes the test
 
-FailLoop: 	
+FailLoop:
 		bra.s	FailLoop				; if the cartridge failed the test, the program ends in this infinite loop
 ; =========================================================================
 
 Test_Registers:
 		dc.l	' SEG'					; d4
-        vdp_comm.l	dc,(vram_fg+((sizeof_vram_row_64*11)+(2*10))),vram,write ; d5 ; VRAM write at $C594 (Line 11, column 10), start location of mappings for first line of license message
+        vdp_comm.l	dc,(vram_fg+((sizeof_vram_row_64*11)+(2*10))),vram,write ; d5 ; VRAM write at $C594 (Line 11, column 10), start location in FG nametable of first line of license message
         dc.l	(sizeof_LicenseFont/4)-1			; d6; loops to copy license message font to VRAM
         dc.l    'SEGA'						; d7
         dc.l    tmss_sega					; a2
@@ -180,17 +187,17 @@ RAM_Code:
 		bset	#0,(a3)					; bankswitch to cartridge
 		cmp.l	(ROM_Header).w,d7			; is 'SEGA' at the start of the ROM header?
 		beq.s	.pass					; if so, cartridge has passed test
-	
+
 		cmp.l	(ROM_Header).w,d4			; if 'SEGA' was not found, try ' SEG' (perhaps accommodating an early game with a typo in the header?)
 		bne.s	.fail					; if that is not found, cartridge has failed the test
 
 		cmpi.b 	#'A',(ROM_Header+4).w			; if ' SEG' was found, check for the last 'A'
 		beq.s	.pass					; if that is found, cartridge has passed test
 
-	.fail: 
+	.fail:
 		bclr	#0,(a3)					; bankswitch back to TMSS ROM
 		move.b	(a6),d0					; get version register
-		andi.b 	#console_revision,d0			; only hardware revision bits	
+		andi.b 	#console_revision,d0			; only hardware revision bits
 		beq.s	.done					; if no VDP lock, branch
 		move.l	#0,(a2)					; lock the VDP
 
@@ -201,6 +208,7 @@ RAM_Code:
 ; If cartridge passed the test, load the message assets and display it for
 ; several seconds.
 ; ---------------------------------------------------------------------------
+
 .pass:
 		bclr	#0,(a3)					; bankswitch back to the TMSS ROM
 		jsr	(LoadPal).l				; copy the palette to CRAM
@@ -211,19 +219,19 @@ RAM_Code:
 		dbf	d6,.load_font
 
 		jsr (Load_Tilemap).l				; copy the ASCII-based tilemap for the license message to VRAM
-		
+
 		move.w	#vdp_enable_display|vdp_md_display,(a4)	; enable display, showing the license message
 		move.w	#$3C,d0
 		bsr.s	DelayLoop				; wait for a few seconds
-		
+
 		move.w	#vdp_md_display,(a4)			; disable display
 		move.b	(a6),d0					; get version register
-		andi.b	#console_revision,d0			; only hardware revision bits	
+		andi.b	#console_revision,d0			; only hardware revision bits
 
 		beq.s	.hand_off_to_cart			; if no VDP lock, branch
-		move.l	#0,(a2)					; lock the VDP (the cartridge will have to unlock it as the second part of the TMSS check)		
-	
-	
+		move.l	#0,(a2)					; lock the VDP (the cartridge will have to unlock it as the second part of the TMSS check)
+
+
 	.hand_off_to_cart:
 		bset	#0,(a3)					; bankswitch back to cartridge (and stay there permanently)
 		moveq	#0,d0					; clear d0
@@ -241,7 +249,7 @@ DelayLoop:							; double-nested loop to delay for a couple seconds while the li
 		dbf d0,DelayLoop				; repeat the inner loop 60 times
 		rts
 
-		arraysize	RAM_Code	
+		arraysize	RAM_Code
 		; End of RAM code
 ; =========================================================================
 
@@ -254,12 +262,12 @@ Pal_Text_Data:
 		incbin	"License Text Font.bin"			;  character set for license message (also includes an unused mini SEGA logo)
 
 	;.license_mappings:
-		dc.b	"   produced by or",endline 
-		dc.b	" under license from",endline 
+		dc.b	"   produced by or",endline
+		dc.b	" under license from",endline
 		dc.b	"sega,enterprises ltd{",endstring	; opening curly brace represents a period, comma may have been meant for a TM symbol or related
 ; =========================================================================
 
-LoadPal: 
+LoadPal:
 		move.w	(a1)+,d0				; set loop counter to the palette size ($1, aka 2 colors)
 		vdp_comm.l	move,2,cram,write,(a4)		; set VDP to CRAM write at Line 1, Entry 3
 
@@ -286,7 +294,7 @@ Load_Tilemap:
 	.next_line:
 		addi.l	#$1000000,d5				; add $1000000 to make VDP command longword to start the next line
 		bra.s	Load_Tilemap				; set new write address
-; =========================================================================		
+; =========================================================================
 
 		dcb.l	19,$FFFFFFFF				; padding
 ROM_End:
